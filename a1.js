d3b569fc73f941c2d3f2779070c8cb8e68ece108
bbashir3967 <67782871+bbashir3967@users.noreply.github.com>
2020-08-25 19:51:38 +0300

    Create a1.js

---
+++ a1.js
@@ -0,0 +1,10 @@
+function showme(data) {
+    // alert(data.Success)
+    obj_to_table1(data.Result)
+    console.log(data)
+    //parseUrls.
+}
+function showme1(data) {
+    // alert(data.Success)
+    console.log(data)
+}
